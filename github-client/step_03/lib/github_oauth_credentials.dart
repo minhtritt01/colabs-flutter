be97b07837429cc833c091a38baa1faa5688acb1
Phan Tri <tri_phan@onskyhealth.com>
2023-11-21 10:47:45 +0700

    add github-client done

---
+++ github-client/step_03/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'f13a66c0bd52a7efc94e';
+const githubClientSecret = 'ed838c170de0cf1ce8a8331c441f64fe1c22f706';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
